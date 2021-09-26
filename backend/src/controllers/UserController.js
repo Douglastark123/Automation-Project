dd2c7b96653abb38c0cb6f5d066fd46aa8743fad
douglastark123 <douglastark123@gmail.com>
2021-09-26 09:14:03 -0400

    simplest/base user CRUD

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,9 +1,70 @@
 const db = require('../database');
 
 module.exports = class User {
-  constructor(req, res) { }
+  constructor(request, response) { }
 
-  index(req, res) {
-    return db('users').then(results => res.json(results))
+  async index(request, response) {
+    const results = await db('users')
+    return response.json(results)
   }
+
+  async create(request, response, next) {
+    try {
+      const { username } = request.body;
+
+      const userExists = await db('users').where('username', username);
+
+      if (userExists) {
+        return response.status(400).json({ "error": `username '${username}' already exists` })
+      }
+
+      await db('users').insert({
+        username
+      })
+
+      return response.status(201).send()
+
+    } catch (error) {
+      next(error)
+    }
+
+  }
+
+  async update(request, response, next) {
+    try {
+      const { username } = request.body;
+      const { id } = request.params;
+
+      await db('users')
+        .update({ username })
+        .where({ id })
+
+      return response.send()
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(request, response, next) {
+    try {
+      const { id } = request.params;
+
+      const userExists = await db('users').where('id', id);
+
+      if (!userExists) {
+        return response.status(400).json({ "error": "there is no user with this id" })
+      }
+
+
+      await db('users')
+        .del()
+        .where({ id })
+
+      return response.send()
+    } catch (error) {
+      next(error);
+    }
+  }
+
+
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,5 +6,8 @@ const user = new UserController;
 const routes = Router();
 
 routes.get('/users', user.index)
+routes.post('/users', user.create)
+routes.put('/users/:id', user.update)
+routes.delete('/users/:id', user.delete)
 
 module.exports = routes;
